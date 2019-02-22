60f43e18eaa037c8db973a90a892d7e8eae5042a
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-02-23 00:06:59 +0300

    Refactoring using inheritance

--- lib/template_method.rb
+++ lib/template_method.rb
@@ -1,25 +1,26 @@
 class Hero
   attr_reader :damage, :abilities
 
-  def initialize(type = nil)
+  def initialize
     @abilities = []
-    if type == :warrior
-      @damage = 15
-      @abilities << :strike
-    elsif type == :mage
-      @damage = 7
-      @abilities << :magic_arrow
-    else
-      @damage = 10
-    end
-  end
-
-  def damage
-    @damage
+    @damage = 10
   end
 
   def attack
     "Attacked dealing #{@damage} damage"
   end
+end
 
+class Warrior < Hero
+  def initialize
+    @damage = 15
+    @abilities = [:strike]
+  end
+end
+
+class Mage < Hero
+  def initialize
+    @damage = 7
+    @abilities = [:magic_arrow]
+  end
 end
